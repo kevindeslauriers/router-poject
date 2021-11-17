203407a200bc561a9a68cded02ba293ce432e524
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2021-11-17 11:14:11 -0500

    Pass JSON

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,36 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link :to="{ name: 'About', params: { msg1 } }">About</router-link>
   </div>
   <router-view/>
 </template>
+
+<script>
+
+export default ({
+ data(){
+   return{
+     msg:Array,
+     msg1:String
+   }
+ },
+ mounted() {
+   this.msg = new Array(3);
+   this.msg[0] = {};
+    this.msg[1] = {};
+    this.msg[2] = {};
+
+   this.msg[0]['test'] = '1';
+   this.msg[0]['id'] = '1234';
+   this.msg[1]['test'] = '1';
+   this.msg[1]['id'] = '1234';
+   this.msg[2]['test'] = '1';
+   this.msg[2]['id'] = '1234';
+   this.msg1 = JSON.stringify(this.msg);
+ },
+})
+</script>
 
 <style>
 #app {
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 @click="test">This is an about page</h1>
+    {{info}}
   </div>
 </template>
+
+<script>
+
+export default {
+  name: 'About',
+  methods: {
+    test(){
+     this.$router.push({path : '/'})
+    }
+  },
+  data() {
+    return {
+      info : Array
+    }
+  },
+  props:['msg1'],
+  mounted() {
+    if (this.msg1){
+      this.info = JSON.parse(this.msg1);
+      console.log(this.info);
+    }
+  }
+}
+</script>
